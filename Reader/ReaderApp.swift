ff93ecf4bdf8330236c45144d58604fdf00a0699
satday <chenchangwei92@gmail.com>
2021-03-27 11:32:52 +0800

    Initial Commit

---
+++ Reader/ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReaderApp.swift
+//  Reader
+//
+//  Created by 陈长炜 on 2021/3/27.
+//
+
+import SwiftUI
+
+@main
+struct ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
